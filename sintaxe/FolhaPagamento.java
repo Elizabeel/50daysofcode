f0d3f7676d71934e4ca19b207e511c50b0f07d05
Elizabeel <104039217+Elizabeel@users.noreply.github.com>
2026-01-20 20:46:36 -0300

    git commit -m "fix: adicionando código na classe"

--- sintaxe/FolhaPagamento.java
+++ sintaxe/FolhaPagamento.java
@@ -1,4 +1,26 @@
 package sintaxe;
 
 public class FolhaPagamento {
+  public static void main(String[] args) {
+
+        Double salarioBase = 2500.0;
+        String horasExtrasTexto = "10";
+
+        int horasExtras = Integer.parseInt(horasExtrasTexto);
+
+        double salarioFinal = calcularSalarioFinal(salarioBase, horasExtras);
+
+        String salarioFinalTexto = Double.toString(salarioFinal);
+
+        System.out.println("Salário final: R$ " + salarioFinalTexto);
+    }
+
+    public static double calcularSalarioFinal(Double salarioBase, int horasExtras) {
+
+        final int VALOR_HORA_EXTRA = 50;
+
+        double valorHorasExtras = horasExtras * VALOR_HORA_EXTRA;
+
+        return salarioBase + valorHorasExtras;
+    }
 }
